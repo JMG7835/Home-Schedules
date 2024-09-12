5d005ee094bd03fa35e718c268844287a1e4c2da
Jean-Michel <jeanmichelgadeaud@hotmail.fr>
2024-09-12 14:38:08 +0200

    Initial commit

---
+++ src/test/java/org/gadconsulting/homeschedules/HomeSchedulesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gadconsulting.homeschedules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeSchedulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
