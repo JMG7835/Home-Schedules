fb2b8736dd11fa24bb72c919ec6c70fde3c44e12
Jean-Michel <jeanmichelgadeaud@hotmail.fr>
2025-05-06 16:50:05 +0200

    feature angular add pages

--- homeschedules/src/app/app.routes.ts
+++ homeschedules/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { ScheduleListComponent } from './pages/schedule-list/schedule-list.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: UserListComponent
+  },
+  {
+    path: 'user',
+    component: UserListComponent
+  },{
+    path: 'schedules',
+    component: ScheduleListComponent
+
+  }
+];
