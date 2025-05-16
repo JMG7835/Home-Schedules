25d01580654545e4ddcc91cc705e196651c5f624
Jean-Michel <jeanmichelgadeaud@hotmail.fr>
2025-05-16 17:11:02 +0200

    feature angular creat user table with paginator

---
+++ homeschedules/src/app/services/schedule.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScheduleService {
+  private apiUrl = 'http://localhost:3000/schedule';
+  constructor(private http: HttpClient) {}
+  getSchedules(page: number, limit: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}?page=${page}&limit=${limit}`);
+  }
